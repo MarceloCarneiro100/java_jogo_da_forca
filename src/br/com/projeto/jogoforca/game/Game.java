02ca58bbeccd3797159fd47e349f0b32e9c1c0f5
Marcelo Carneiro <marcelo_tech100@hotmail.com>
2026-01-07 18:58:49 -0300

    implement Dictionary class with word loading logic

--- src/br/com/projeto/jogoforca/game/Game.java
+++ src/br/com/projeto/jogoforca/game/Game.java
@@ -1,16 +1,14 @@
 package br.com.projeto.jogoforca.game;
 
+import br.com.projeto.jogoforca.core.Dictionary;
 import br.com.projeto.jogoforca.core.Word;
 
 public class Game {
 
 	public void start() {
-		Word word = new Word("casa");
-		word.hasChar('a');
-		word.hasChar('s');
-		word.hasChar('c');
-
-		System.out.println(word);
-		System.out.println(word.discovered());
+		Dictionary dictionary = new Dictionary();
+		Word word = dictionary.nextWord();
+		
+		System.out.println(word.getOriginalWord());
 	}
 }
---
+++ src/br/com/projeto/jogoforca/game/GameException.java
@@ -0,0 +1,9 @@
+package br.com.projeto.jogoforca.game;
+
+@SuppressWarnings("serial")
+public class GameException extends RuntimeException {
+
+	public GameException(String message) {
+		super(message);
+	}
+}
