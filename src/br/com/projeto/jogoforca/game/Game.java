9fc87f39225809ebb59dc8faf4bb15ff2bda1164
Marcelo Carneiro <marcelo_tech100@hotmail.com>
2026-01-07 18:21:03 -0300

    implement Word class with hidden characters

--- src/br/com/projeto/jogoforca/game/Game.java
+++ src/br/com/projeto/jogoforca/game/Game.java
@@ -1,8 +1,16 @@
 package br.com.projeto.jogoforca.game;
 
+import br.com.projeto.jogoforca.core.Word;
+
 public class Game {
 
 	public void start() {
+		Word word = new Word("casa");
+		word.hasChar('a');
+		word.hasChar('s');
+		word.hasChar('c');
 
+		System.out.println(word);
+		System.out.println(word.discovered());
 	}
 }
