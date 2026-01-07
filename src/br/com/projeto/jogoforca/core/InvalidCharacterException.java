196963ad46a4a2745007918052b14b1c79bb8aad
Marcelo Carneiro <marcelo_tech100@hotmail.com>
2026-01-07 19:35:14 -0300

    add UI class for input/output handling

---
+++ src/br/com/projeto/jogoforca/core/InvalidCharacterException.java
@@ -0,0 +1,9 @@
+package br.com.projeto.jogoforca.core;
+
+@SuppressWarnings("serial")
+public class InvalidCharacterException extends Exception {
+
+	public InvalidCharacterException(String message) {
+		super(message);
+	}
+}
